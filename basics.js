22d235ef245ccb26031fcc95640c321c68f3ece2
stephen <step5790@stud.kea.dk>
2021-08-29 12:02:01 +0200

    initial files

--- basics.js
+++ basics.js
@@ -1,1 +1,33 @@
-console.log("basics");
+"use strict";
+
+const bool= true;
+const num=13;
+const str = "1";
+const str1 = "peter";
+const obj = ["cat:1", "cat1:2"];
+const obj1 = {dog:1, dog1:2};
+const nothing=null;
+let unf;
+const symbol = Symbol("symbol");
+
+
+console.log(bool);
+console.log(num);
+console.log(str1);
+console.log(obj1);
+
+obj.forEach(function(entry) {
+    console.log(entry);
+});
+
+
+
+console.log(nothing);
+console.log(unf);
+console.log(symbol);
+
+// convert string to a number
+
+console.log(Number(str)+Number(str));
+console.log(num+num);
+
---
+++ worstCalculator/script.js
@@ -0,0 +1,41 @@
+"usestrict";
+let sum;
+
+window.addEventListener("DOMContentLoaded", clickCalculate);
+
+function clickCalculate() {
+  console.log("calculating");
+
+  document.querySelector("#calculate").addEventListener("click", () => {
+    let firstnumber = document.getElementById("firstnumber").value;
+    let secondnumber = document.getElementById("secondnumber").value;
+    let operator = document.getElementById("operator").value;
+
+    console.log(firstnumber, operator, secondnumber);
+
+    if (operator === "sub") {
+      sum = parseInt(firstnumber) - parseInt(secondnumber);
+    } else if (operator === "add") {
+      sum = parseInt(firstnumber) + parseInt(secondnumber);
+    } else if (operator === "div") {
+      sum = parseInt(firstnumber) / parseInt(secondnumber);
+    } else if (operator === "mul") {
+      sum = parseInt(firstnumber) * parseInt(secondnumber);
+    }
+    console.log(sum);
+    roundNumber();
+  });
+}
+function roundNumber() {
+  console.log("roundnumber");
+  const checked = document.getElementById("doround").checked;
+  if (checked === true) {
+    const decimal = document.getElementById("decimals").value;
+    console.log(sum.toFixed(decimal));
+
+    let x = document.querySelector("li");
+    let y = document.createTextNode(sum);
+    x.appendChild(y);
+    document.getElementById("results").appendChild(y);
+  }
+}
